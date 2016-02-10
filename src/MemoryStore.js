535454e8938c3fa9e137b2caa57544c20d79d1c5
Michiel van der Velde <michiel@michielvdvelde.nl>
2016-02-10 15:17:25 +0100

    Add support for JSON values

--- src/MemoryStore.js
+++ src/MemoryStore.js
@@ -10,11 +10,33 @@ export class MemoryStore {
   }
 
   /**
+   * Parse valid JSON or return original value
+  **/
+  _tryParseJSON(str) {
+    try {
+      str = JSON.parse(str);
+    }
+    catch(e) { }
+    return str;
+  }
+
+  /**
+   * Stringify JSON or return original value
+  **/
+  _tryStringifyJSON(obj) {
+    try {
+      obj = JSON.stringify(obj);
+    }
+    catch(e) { }
+    return obj;
+  }
+
+  /**
    * Set a key to a value
   **/
   set(key, value, override = true) {
     if((!this._store[key]) || (this._store[key] && override)) {
-      this._store[key] = value;
+      this._store[key] = this.__tryStringifyJSON(value);
     }
   }
 
@@ -23,7 +45,7 @@ export class MemoryStore {
   **/
   get(key, def = null) {
     if(this._store[key]) {
-      return this._store[key];
+      return this._tryParseJSON(this._store[key]);
     }
     return def;
   }
