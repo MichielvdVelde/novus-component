7ca92048e772a4e144daadcccea52cd18f881789
Michiel van der Velde <michiel@michielvdvelde.nl>
2016-03-17 15:11:04 +0100

    Replace extend with default options const

--- src/lib/Route.js
+++ src/lib/Route.js
@@ -30,10 +30,7 @@ export class Route {
 
     this._topic = mqtt_regex(route.topic);
     this._handler = route.handler;
-    this._options = extend({
-      subscribe: true,
-      qos: 0
-    }, route.options || {});
+    this._options = extend(DEFAULT_OPTIONS, route.options || {});
   }
 
   /**
